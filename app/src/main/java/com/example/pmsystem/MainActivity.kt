a8a751fc745d0fa2e13d02def587080e99e6f982
Onil Pravasi <onilpravasi@yahoo.com>
2019-06-01 13:23:27 -0500

    Added bottom navigation and fragment container

--- app/src/main/java/com/example/pmsystem/MainActivity.kt
+++ app/src/main/java/com/example/pmsystem/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.pmsystem
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
+import android.widget.TextView
+import com.example.pmsystem.R.id.nav_view
+import com.example.pmsystem.R.id.message as message
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
