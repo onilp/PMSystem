5d00b43337d8c372aefb19d12a668def93c44f0e
Calvert <wongcalvert@gmail.com>
2019-06-02 16:36:13 -0500

    Made bottom navigation drawer to work

--- app/src/main/java/com/example/pmsystem/MainActivity.kt
+++ app/src/main/java/com/example/pmsystem/MainActivity.kt
@@ -2,19 +2,15 @@ package com.example.pmsystem
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
-import android.widget.TextView
-import android.widget.Toast
-import com.example.pmsystem.R.id.home
-import com.example.pmsystem.R.id.nav_view
+import com.example.pmsystem.util.bottomnavigationdrawer.BottomNavClickListener
+import com.example.pmsystem.util.bottomnavigationdrawer.BottomNavFragment
 import org.jetbrains.anko.toast
 import com.example.pmsystem.R.id.message as message
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    BottomNavClickListener {
 
     lateinit var fragmentManager: FragmentManager
 
@@ -22,51 +18,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-
         isUserLoggedIn()
 
     }
 
     // this function takes in a new fragment and do the fragment transaction
     // main purpose of this is to reduce the amount of boiler plate code while doing fragment transaction
-    private fun fragmentTransactionHandler(fragment: Fragment) {
+    private fun fragmentReplaceHandler(fragment: Fragment) {
         fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
     }
 
-    private val onNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.nav_home -> {
-                val homeFragment = HomeFragment.newInstance()
-                fragmentTransactionHandler(homeFragment)
-                // this line is required so that the animation works on selected
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.nav_create_project -> {
-                val createProjectFragment = CreateProjectFragment.newInstance()
-                fragmentTransactionHandler(createProjectFragment)
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.nav_something -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+    private fun fragmentAddHandler(fragment: Fragment){
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().add(R.id.fragment_bottom_nav, fragment).commit()
     }
 
+    private fun fragmentRemoveHandler(fragment: Fragment){
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().remove(fragment).commit()
+    }
+
+    // check if user is already logged in
     private fun isUserLoggedIn() {
+        //TODO: check shared preferences (Bin)
         if(true){
             val homeFragment = HomeFragment.newInstance()
-            fragmentTransactionHandler(homeFragment)
+            fragmentReplaceHandler(homeFragment)
+
+            // display bottom nav drawer if user is logged in
+            val bottomNavFragment = BottomNavFragment.newInstance()
+            fragmentAddHandler(bottomNavFragment)
         }else{
-            //login fragment
-
-
-            // this code is needed to remove the bottom navigation drawer in login fragment
-
+            fragmentManager = supportFragmentManager
+            // remove bottom nav fragment if it is on the screen and user is not logged in
+            val bottomNavFragment = fragmentManager.findFragmentById(R.id.fragment_bottom_nav)
+            if(bottomNavFragment != null){
+                fragmentRemoveHandler(bottomNavFragment)
+            }else{
+                toast("No bottom nav is removed")
+            }
         }
+    }
+
+    override fun onBottonNavClicked(fragment: Fragment) {
+        fragmentReplaceHandler(fragment)
     }
 }
---
+++ app/src/main/java/com/example/pmsystem/util/bottomnavigationdrawer/BottomNavFragment.kt
@@ -0,0 +1,74 @@
+package com.example.pmsystem.util.bottomnavigationdrawer
+
+import android.content.Context
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pmsystem.CreateProjectFragment
+import com.example.pmsystem.HomeFragment
+import com.example.pmsystem.R
+
+class BottomNavFragment : Fragment() {
+
+    lateinit var bottomNavClickListener: BottomNavClickListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_bottom_nav, container, false)
+
+        val navView: BottomNavigationView = view!!.findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        return view
+    }
+
+    companion object {
+        fun newInstance(): BottomNavFragment {
+            val bottomNavFragment =
+                BottomNavFragment()
+            return bottomNavFragment
+        }
+    }
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // instantiate a fragment
+                    val homeFragment = HomeFragment.newInstance()
+                    // send the fragment as argument to MainActivity and replace the fragment container with this fragment
+                    bottomNavClickListener.onBottonNavClicked(homeFragment)
+
+                    // this line is required so that the animation works on selected
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_create_project -> {
+                    val createProjectFragment =
+                        CreateProjectFragment.newInstance()
+                    bottomNavClickListener.onBottonNavClicked(createProjectFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_something -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is BottomNavClickListener) {
+            // set bottom nav click listener to this fragment
+            bottomNavClickListener = context
+        } else {
+            throw ClassCastException(context.toString() + getString(R.string.bottom_nav_exception_message))
+        }
+    }
+
+}
