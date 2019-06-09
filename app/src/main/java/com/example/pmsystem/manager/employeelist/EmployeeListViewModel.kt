6d7550dfafd391c231a76eb1e8747ea9c46ff2f6
Calvert <wongcalvert@gmail.com>
2019-06-09 12:52:57 -0500

    Fetch employee

--- app/src/main/java/com/example/pmsystem/manager/employeelist/EmployeeListViewModel.kt
+++ app/src/main/java/com/example/pmsystem/manager/employeelist/EmployeeListViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.pmsystem.manager.employeelist
 
 import android.annotation.SuppressLint
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.example.pmsystem.MyApplication
+import com.example.pmsystem.model.employee.Employee
+import com.example.pmsystem.model.employee.EmployeeResponse
 import com.example.pmsystem.network.ApiInterface
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -15,17 +18,25 @@ class EmployeeListViewModel: ViewModel(), AnkoLogger {
     @Inject
     lateinit var apiInterface: ApiInterface
 
+    lateinit var employeeList: MutableLiveData<List<Employee>>
+
     @SuppressLint("CheckResult")
-    fun getEmployeeList(){
+    fun fetchEmployee(): MutableLiveData<List<Employee>>{
         MyApplication.component.inject(this)
+
+        employeeList = MutableLiveData()
 
         apiInterface.getEmployeeList()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 debug(it.toString())
+                employeeList.value = it.employees
             }, {
                 error(it.message)
+                employeeList.value = null
             })
+
+        return employeeList
     }
 }
